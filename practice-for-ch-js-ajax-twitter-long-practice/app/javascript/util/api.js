a551403ad2f943bab38a7e5cca02b156a46be8b3
wtdavis <wtdavis@wesleyan.edu>
2023-03-08 15:17:14 -0500

    milner chen

--- practice-for-ch-js-ajax-twitter-long-practice/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice/app/javascript/util/api.js
@@ -3,10 +3,23 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
+    'X-CSRF-Token': csrfToken,
     ...options.headers
   };
 
   return await fetch(url, options);
 }
 
+function followUser(id) {
+  return customFetch(`/users/${id}/follow`, {method: "POST"});
+
+}
+
+function unfollowUser(id) {
+  return customFetch(`/users/${id}/follow`, {method: "DELETE"});
+
+}
+
+export const unfollow =  unfollowUser;
+export const follow = followUser;
 export const foo = "bar";
